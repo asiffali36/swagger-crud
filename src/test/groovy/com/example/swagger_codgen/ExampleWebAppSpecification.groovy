406cea4bfd493e5691d1c9a1a12aa7a83f037701
asiffali36 <asiffali36@gmail.com>
2022-09-15 15:00:57 +0500

    added crud test code

--- src/test/groovy/com/example/swagger_codgen/ExampleWebAppSpecification.groovy
+++ src/test/groovy/com/example/swagger_codgen/ExampleWebAppSpecification.groovy
@@ -1,4 +1,5 @@
 package com.example.swagger_codgen
+
 import groovyx.net.http.RESTClient
 import groovyx.net.http.Status
 import spock.lang.Specification
@@ -7,31 +8,64 @@ import spock.lang.Specification
 
 class ExampleWebAppSpecification extends Specification {
     def primerEndpoint = new RESTClient('http://localhost:8080')
+    int userid;
 
-
-    def 'should return 201 code (created) when trying to save record with all required fields'() {
+    def 'create Test'() {
         when: 'try to save record with all required fields'
         def response = primerEndpoint.post(
                 path: '/update-employeedetails',
                 body: [
-                           "firstName" : "another",
-                           "lastName": "record"
+                        "firstName" : "another",
+                        "lastName": "record"
 
                 ],
                 requestContentType: 'application/json'
         )
 
         then: 'server returns 201 code (created)'
-       // println (response.toString())
+
+        def  id=  response.responseData.body
+        int userid = (Integer) id;
+
         with(response) {
+
             Status.SUCCESS
         }
 
     }
 
-    def 'Should return 200 & a message with the input appended'() {
+    def 'Read Test'() {
         when: 'Checking credentials before getting data'
         def resp = primerEndpoint.get(path: '/employee-details')
+        then:
+        with(resp) {
+            Status.SUCCESS
+
+        }
+    }
+    def 'Update Test'() {
+
+        when: 'try to save record with all required fields'
+        def response = primerEndpoint.put(
+                path: '/update-employeedetails',
+                body: [
+                        "firstName" : "updated",
+                        "lastName": "record"
+
+                ],
+                query : [ id : userid ],
+                requestContentType: 'application/json'
+        )
+
+        then: 'server returns 204 code (updated)'
+        with(response) {
+            Status.SUCCESS
+        }
+
+    }
+    def 'Delete testing'() {
+        when: 'Checking credentials before getting data'
+        def resp = primerEndpoint.delete( path: '/delete-employee', query : [ id : userid ])
         then:
         with(resp) {
             Status.SUCCESS
